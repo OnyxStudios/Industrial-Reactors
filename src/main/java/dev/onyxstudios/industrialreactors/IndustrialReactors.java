57d02dab65fc35eebd5eab828a1eacbca89f1d48
UpcraftLP <j-schirra@gmx.de>
2020-09-14 01:58:41 +0200

    set modid

--- src/main/java/dev/onyxstudios/industrialreactors/IndustrialReactors.java
+++ src/main/java/dev/onyxstudios/industrialreactors/IndustrialReactors.java
@@ -2,6 +2,7 @@ package dev.onyxstudios.industrialreactors;
 
 import io.github.glasspane.mesh.api.logging.MeshLoggerFactory;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.Logger;
 
 public class IndustrialReactors implements ModInitializer {
@@ -9,6 +10,10 @@ public class IndustrialReactors implements ModInitializer {
     public static final String MODID = "industrial_reactors";
 
     private static final Logger log = MeshLoggerFactory.createPrefixLogger(MODID, "Industrial Reactors");
+
+    public static Identifier id(String path) {
+        return new Identifier(MODID, path);
+    }
 
     public static Logger getLogger() {
         return log;
